de79ad1849b1ad0b002acc7ae5e242a29bada54f
Chris9981jubi <149410191+Chris9981jubi@users.noreply.github.com>
2024-09-14 13:59:51 +0530

    Update storeContext.jsx

--- frontend/src/context/storeContext.jsx
+++ frontend/src/context/storeContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
         const savedCart = localStorage.getItem("cartItems");
         return savedCart ? JSON.parse(savedCart) : {};
     });
-    const url ="http://localhost:4000"
+    const url ="https://food-delivery-back-lr81.onrender.com"
     const [token,setToken]=useState("");
     const[food_list, setFoodlist]= useState([])
 
