31f5e88dc7ff69092828d7494f2abb61372da8e4
Chris9981jubi <149410191+Chris9981jubi@users.noreply.github.com>
2024-09-14 14:02:37 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -3,6 +3,8 @@ import order_icon from "./order_icon.png"
 import add_icon from "./add_icon.jpg"
 import Parcel from "./Parcel.png"
 
+export const url ="https://food-delivery-back-lr81.onrender.com"
+
 export const assets={
     profile_pic,
     order_icon,
